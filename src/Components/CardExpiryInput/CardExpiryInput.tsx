b4d9800b1ab9e0e8c8e46f095657cc9f8b7a02e4
river <jenn0.6n@gmail.com>
2024-05-05 21:49:07 +0900

    Refactor: CardExpiryInput valid,error 세분화

--- src/Components/CardExpiryInput/CardExpiryInput.tsx
+++ src/Components/CardExpiryInput/CardExpiryInput.tsx
@@ -20,19 +20,17 @@ const CardExpiryInput: React.FC<CardExpiryInputProps> = ({
   setExpiryMonthCompleted,
   setExpiryYearCompleted,
 }) => {
-  const [isValid, setIsValid] = useState(true);
+  const [isMonthValid, setIsMonthValid] = useState(true);
+  const [isYearValid, setIsYearValid] = useState(true);
   const [errorMessage, setErrorMessage] = useState("");
-  const inputRefs = useRef<Array<HTMLInputElement | null>>([]);
-
-  const handleValidate = (isValid: boolean) => {
-    setIsValid(isValid);
-  };
+  const monthRef = useRef<HTMLInputElement>(null);
+  const yearRef = useRef<HTMLInputElement>(null);
 
   const handleMonthChange = (inputValue: string) => {
     onMonthChange(inputValue);
 
     if (inputValue.length === 2) {
-      inputRefs.current[1]?.focus();
+      yearRef.current?.focus();
     }
   };
 
@@ -42,77 +40,77 @@ const CardExpiryInput: React.FC<CardExpiryInputProps> = ({
 
   const monthValidator = (value: string) => {
     if (!/^\d*$/.test(value)) {
-      setIsValid(false);
+      setIsMonthValid(false);
       setErrorMessage("유효 기간은 숫자만 입력 가능합니다.");
       return false;
     }
 
     if (Number(value) > 12) {
-      setIsValid(false);
+      setIsMonthValid(false);
       setErrorMessage("달은 12까지만 가능합니다.");
       return false;
     }
 
-    setIsValid(true);
+    setIsMonthValid(true);
+    setErrorMessage("");
+    return true;
+  };
+
+  const yearValidator = (value: string) => {
+    const currentYear = Number(new Date().getFullYear().toString().slice(2));
+
+    if (!/^\d*$/.test(value)) {
+      setIsYearValid(false);
+      setErrorMessage("유효 기간은 숫자만 입력 가능합니다.");
+      return false;
+    }
+
+    if (Number(value) < currentYear) {
+      setIsYearValid(false);
+      setErrorMessage(`${currentYear}년 이후만 가능합니다.`);
+      return false;
+    }
+
+    setIsYearValid(true);
     setErrorMessage("");
     return true;
   };
 
   useEffect(() => {
-    if (isValid && month && year) {
+    if (isMonthValid && isYearValid && month && year) {
       setExpiryMonthCompleted(true);
       setExpiryYearCompleted(true);
     } else {
       setExpiryMonthCompleted(false);
       setExpiryYearCompleted(false);
     }
-  }, [isValid, month, year]);
-
-  const yearValidator = (value: string) => {
-    const currentYear = Number(new Date().getFullYear().toString().slice(2));
-
-    if (!/^\d*$/.test(value)) {
-      setIsValid(false);
-      setErrorMessage("유효 기간은 숫자만 입력 가능합니다.");
-      return false;
-    }
-
-    if (Number(value) < currentYear) {
-      setIsValid(false);
-      setErrorMessage(`${currentYear}년 이후만 가능합니다.`);
-      return false;
-    }
-
-    setIsValid(true);
-    setErrorMessage("");
-    return true;
-  };
+  }, [isMonthValid, isYearValid, month, year]);
 
   return (
     <>
       <ExpiryInputWrapper>
         <Input
-          ref={(el) => (inputRefs.current[0] = el)}
+          ref={monthRef}
           value={month}
           onChange={(month) => handleMonthChange(month)}
-          onValidate={(isValid) => handleValidate(isValid)}
+          onValidate={(isValid) => setIsMonthValid(isValid)}
           maxLength={2}
           placeholder="MM"
           size="medium"
           validator={(value) => monthValidator(value)}
         />
         <Input
-          ref={(el) => (inputRefs.current[1] = el)}
+          ref={yearRef}
           value={year}
           onChange={(year) => handleYearChange(year)}
-          onValidate={(isValid) => handleValidate(isValid)}
+          onValidate={(isValid) => setIsYearValid(isValid)}
           maxLength={2}
           placeholder="YY"
           size="medium"
           validator={(value) => yearValidator(value)}
         />
       </ExpiryInputWrapper>
-      <Tooltip>{!isValid ? errorMessage : ""}</Tooltip>
+      <Tooltip>{!isMonthValid || !isYearValid ? errorMessage : ""}</Tooltip>
     </>
   );
 };
